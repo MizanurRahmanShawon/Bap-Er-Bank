cf584ec9670decb7a39a8be1a8334905376277e6
MizanurRahmanShawon <mr630961@gmail.com>
2023-02-16 23:32:47 +0600

    Update deposit and balance and handle string add

--- deposit.js
+++ deposit.js
@@ -1,10 +1,30 @@
 document.getElementById("btn-deposit").addEventListener("click", function () {
   const depositField = document.getElementById("deposit-field");
-  const depositAmount = depositField.value;
+  const newDepositAmountString = depositField.value;
+  const newDepositAmount = parseFloat(newDepositAmountString);
+  // console.log(typeof newDepositAmount);
   // console.log(depositAmount);
   // step-3
   const depositTotalElement = document.getElementById("deposit-total");
-  const depositTotal = depositTotalElement.innerText;
+  const previousDepositTotalString = depositTotalElement.innerText;
+  const previousDepositTotal = parseFloat(previousDepositTotalString);
+  console.log(typeof previousDepositTotal);
   // console.log(depositTotal);
-  depositTotalElement.innerText = depositAmount;
+
+  // step-4
+  const currentDepositTotal = previousDepositTotal + newDepositAmount;
+  depositTotalElement.innerText = currentDepositTotal;
+
+  // step-5 get balance total
+  const balanceTotalElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  // step-6  calculate total balance show
+  const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+  // set the balance total
+  balanceTotalElement.innerText = currentBalanceTotal;
+
+  // step-7: clear the deposit field
+  depositField.value = "";
 });
