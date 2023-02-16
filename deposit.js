fc934cb18581842eb8ab6c3af5f58346b340a66b
MizanurRahmanShawon <mr630961@gmail.com>
2023-02-16 22:16:13 +0600

    set deposit total value

--- deposit.js
+++ deposit.js
@@ -1,5 +1,10 @@
-// document.getElementById("btn-deposit").addEventListener("click", function () {
-//   const depositField = document.getElementById("deposit-field");
-//   const depositField = depositField.Value;
-//   console.log(depositField);
-// });
+document.getElementById("btn-deposit").addEventListener("click", function () {
+  const depositField = document.getElementById("deposit-field");
+  const depositAmount = depositField.value;
+  // console.log(depositAmount);
+  // step-3
+  const depositTotalElement = document.getElementById("deposit-total");
+  const depositTotal = depositTotalElement.innerText;
+  // console.log(depositTotal);
+  depositTotalElement.innerText = depositAmount;
+});
